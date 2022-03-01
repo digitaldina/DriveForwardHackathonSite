fc1ef39e63aa386fcebf10131ccb1d3e9feb6280
digitaldina <dinaelhanan@gmail.com>
2022-03-01 12:57:11 -0500

    remove unnecessary getinitialprops

--- pages/_app.js
+++ pages/_app.js
@@ -64,16 +64,7 @@ const fonts = `
 `
 
 export default class extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
+  
   render() {
     const { Component, pageProps } = this.props
 
