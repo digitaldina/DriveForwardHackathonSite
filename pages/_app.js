3dcfde1c395f85df0776b55a0a507c610c037016
digitaldina <dinaelhanan@gmail.com>
2022-03-01 12:58:56 -0500

    nvm

--- pages/_app.js
+++ pages/_app.js
@@ -64,7 +64,16 @@ const fonts = `
 `
 
 export default class extends App {
-  
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getStaticProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
   render() {
     const { Component, pageProps } = this.props
 
